2f6c4ffc751613ca315f4bbf842a403c53afa86d
anchu <t.chulinsky@gmail.com>
2019-12-10 10:28:14 +0200

    Project transferred to git

---
+++ src/js/services/apiService.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import config from '../config/apiConfig';
+
+/*
+ * /countries - array of countries
+ * /cities - array of cities
+ * /prices/cheap - array
+ */
+class Api {
+  constructor(config) {
+    /*get link API from file apiConfig*/
+    this.url = config.url;
+  }
+  async countries() {
+    try {
+      /*get response from file apiConfig about available countries*/
+      const response = await axios.get(`${this.url}/countries`);
+      return response.data;
+    } catch (err) {
+      console.log(err);
+      return Promise.reject(err);
+    }
+  }
+  async cities() {
+    try {
+      /*get response from file apiConfig about available cities*/
+      const response = await axios.get(`${this.url}/cities`);
+      return response.data;
+    } catch (err) {
+      console.log(err);
+      return Promise.reject(err);
+    }
+  }
+  async airlines() {
+    try {
+      /*get response from file apiConfig about available airlines*/
+      const response = await axios.get(`${this.url}/airlines`);
+      return response.data;
+    } catch (err) {
+      console.log(err);
+      return Promise.reject(err);
+    }
+  }
+  async prices(params) {
+    try {
+      const response = await axios.get(`${this.url}/prices/cheap`, {
+        params,
+      });
+      return response.data;
+    } catch (err) {
+      console.log(err);
+      return Promise.reject(err);
+    }
+  }
+}
+
+const api = new Api(config);
+
+export default api;
